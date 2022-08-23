82e32419fb07bd5bbaa710ddad781986efaef473
eric <lee.eric.03@gmail.com>
2022-08-23 16:45:09 -0600

    game, board, square setup/styling. Refactors board to render square components. squares are indexed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css'
+
+// const gameStyles = {
+//   background: '#f4a261',
+//   margin: 10,
+//   padding: 20,
+// }
+
+const Square = (props) => {
+  return (
+    <div className="square">
+      {props.value}
+    </div>
+  )
+}
+
+const Board = () => {
+  const renderSquare = (i) => {
+    return (
+      <Square value={i} />
+    )
+  }
+
+  return (
+    <div className="board">
+      Board
+      <div className="board-row">
+        {renderSquare(1)}{renderSquare(2)}{renderSquare(3)}
+      </div>
+      <div className="board-row">
+        {renderSquare(4)}{renderSquare(5)}{renderSquare(6)}
+      </div>
+      <div className="board-row">
+        {renderSquare(7)}{renderSquare(8)}{renderSquare(9)}
+      </div>
+    </div>
+  )
+}
+
 
 const Game = () => {
   return (
-    <div>
+    <div className="game">
       Game
+      <Board />
     </div>
   )
 };
